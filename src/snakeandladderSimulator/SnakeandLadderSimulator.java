485e71fe91aa3049614daa0f1dc47d9bdc52fca5
Soun9922 <sagsounak39@gmail.com>
2022-05-15 06:33:59 +0530

    Implemetation of case in program for the player check for options

--- src/snakeandladderSimulator/SnakeandLadderSimulator.java
+++ src/snakeandladderSimulator/SnakeandLadderSimulator.java
@@ -1,10 +1,32 @@
 package snakeandladderSimulator;
 
 public class SnakeandLadderSimulator {
-    final static int startngPosition = 0;
-    final static int numberofPlayers = 1;
     public static void main(String[] args) {
         System.out.println("-----Welcome to Snake and Ladder Simulator-----");
+         int startngPosition = 0;
+         int numberofPlayers = 1;
+         int finalPosition = 10;
+         int noPlay = 0;
+         int counter = 0;
+         int ladder = 1;
+         int snake = 2;
+         int playerPos = startngPosition;
         int diceRoll = (int)(Math.random() * 6) + 1;
+        int optionChoice = (int)(Math.random() * 3);
+        switch (optionChoice){
+            case 0:
+                playerPos = playerPos;
+                System.out.println("The player stays in the same position");
+                break;
+            case 1:
+                playerPos = playerPos + diceRoll;
+                System.out.println("The player moves ahead");
+                break;
+            case 2:
+                playerPos = playerPos - diceRoll;
+                System.out.println("The player moves behind");
+
+        }
+
     }
 }
