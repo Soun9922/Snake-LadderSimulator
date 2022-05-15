6d039aaaa6b99f58a5e45f7c706ec0e666c2a62d
Soun9922 <sagsounak39@gmail.com>
2022-05-15 05:48:48 +0530

    The Values are defined.

--- src/snakeandladderSimulator/SnakeandLadderSimulator.java
+++ src/snakeandladderSimulator/SnakeandLadderSimulator.java
@@ -1,7 +1,7 @@
 package snakeandladderSimulator;
 
 public class SnakeandLadderSimulator {
-    final static int startPosition = 0;
+    final static int startngPosition = 0;
     final static int numberofPlayers = 1;
     public static void main(String[] args) {
         System.out.println("-----Welcome to Snake and Ladder Simulator-----");
