cc94b390fc130c95935787bd2ecafdf75c5d5a44
Soun9922 <sagsounak39@gmail.com>
2022-05-15 07:01:17 +0530

    Checking the player till reaches to winning position

--- src/snakeandladderSimulator/SnakeandLadderSimulator.java
+++ src/snakeandladderSimulator/SnakeandLadderSimulator.java
@@ -3,17 +3,17 @@ package snakeandladderSimulator;
 public class SnakeandLadderSimulator {
     public static void main(String[] args) {
         System.out.println("-----Welcome to Snake and Ladder Simulator-----");
-         int startngPosition = 0;
-         int numberofPlayers = 1;
-         int finalPosition = 10;
-         int noPlay = 0;
-         int counter = 0;
-         int ladder = 1;
-         int snake = 2;
-         int playerPos = startngPosition;
-        int diceRoll = (int)(Math.random() * 6) + 1;
-        int optionChoice = (int)(Math.random() * 3);
-        switch (optionChoice){
+        int startngPosition = 0;
+        int numberofPlayers = 1;
+        int finalPosition = 100;
+        int noPlay = 0;
+        int counter = 0;
+        int ladder = 1;
+        int snake = 2;
+        int playerPos = startngPosition;
+        int diceRoll = (int) (Math.random() * 6) + 1;
+        int optionChoice = (int) (Math.random() * 3);
+        switch (optionChoice) {
             case 0:
                 playerPos = playerPos;
                 System.out.println("The player stays in the same position");
@@ -25,8 +25,15 @@ public class SnakeandLadderSimulator {
             case 2:
                 playerPos = playerPos - diceRoll;
                 System.out.println("The player moves behind");
-
+                if (playerPos <= startngPosition) {
+                    playerPos = startngPosition;
+                    System.out.println("The Player need to restart the game");
+                } else if (playerPos <= finalPosition) {
+                    playerPos = finalPosition;
+                }
+                while (playerPos < finalPosition) {
+                    System.out.println("PLAY GAME");
+                }
         }
-
     }
 }
