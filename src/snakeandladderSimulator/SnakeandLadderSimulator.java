df7aa5a6e1c3de5554df1a9a352739e6dbc46d38
Soun9922 <sagsounak39@gmail.com>
2022-05-15 05:42:12 +0530

    Define initial value

---
+++ src/snakeandladderSimulator/SnakeandLadderSimulator.java
@@ -0,0 +1,9 @@
+package snakeandladderSimulator;
+
+public class SnakeandLadderSimulator {
+    final static int startPosition = 0;
+    final static int numberofPlayers = 1;
+    public static void main(String[] args) {
+        System.out.println("-----Welcome to Snake and Ladder Simulator-----");
+    }
+}
